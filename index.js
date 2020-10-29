83e90c0b97e1031062f1090776aebabae4f5e541
Shoji <ssuzuki@rx.tnc.ne.jp>
2020-10-29 11:44:16 +0000

    HTMLを表示

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTML‚Ìˆê”Ô‘å‚«‚¢Œ©o‚µ‚ğ•\¦‚µ‚Ü‚·</h1></body></html>');
+    res.end();
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+    console.log('Listening on ' + port);
 });
